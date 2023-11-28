6a263b887433995e285cb823aeaf489a619e044d
WadimSh <wadim11@yandex.ru>
2023-11-28 14:57:57 +0300

    fix: внесены исправления для улучшения семантики

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(fileUpload());
 app.use(express.json());
 
 const imagesPath = path.join(__dirname, "images"); // Путь к папке images
-const maxSize = 5 * 1024 * 1024; // Максимальный размер файла (в данном случае 5MB)
+const maxSize = 1 * 1024 * 1024; // Максимальный размер файла (в данном случае 1MB)
 
 // Создаем папку images, если она не существует
 if (!fs.existsSync(imagesPath)) {
@@ -29,44 +29,36 @@ const findByName = async (dir, name) => {
   return matchedFiles;
 };
 
+
 // Обработчик загрузки изображений
 app.post("/upload/:folder", (req, res) => {
   const { folder } = req.params;
   if (!req.files || !req.files.image) {
     return res.status(400).json({ error: "No image found" });
   }
-
   const image = req.files.image;
   const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
-
   // Проверка типа файла
   if (!allowedTypes.includes(image.mimetype)) {
     return res.status(400).json({ error: "Invalid file type" });
   }
-
   // Проверка размера файла
   if (image.size > maxSize) {
-    return res
-      .status(400)
-      .json({ error: `File is too large (max size: ${maxSize} bytes)` });
+    return res.status(400).json({ error: `File is too large (max size: ${maxSize} bytes)` });
   }
-
   const filename = `${uuidv4()}-${image.name}`;
-
   image.mv(path.join(imagesPath, folder, filename), (err) => {
     if (err) {
       return res.status(500).json({ error: "Failed to upload image" });
     }
-
-    res.json({ filename });
+    res.json({ folder, filename });
   });
 });
 
-// Получение ссылки на изображение по имени файла
+// Получение изображения по имени файла
 app.get("/image/:folder/:filename", (req, res) => {
   const { folder, filename } = req.params;
   const imagePath = path.join(imagesPath, folder, filename);
-
   // Проверка существования изображения
   if (fs.existsSync(imagePath)) {
     res.sendFile(imagePath);
@@ -79,7 +71,6 @@ app.get("/image/:folder/:filename", (req, res) => {
 app.delete("/image/:folder/:filename", (req, res) => {
   const { folder, filename } = req.params;
   const imagePath = path.join(imagesPath, folder, filename);
-
   // Проверка существования изображения
   if (fs.existsSync(imagePath)) {
     fs.unlinkSync(imagePath);
@@ -90,23 +81,27 @@ app.delete("/image/:folder/:filename", (req, res) => {
 });
 
 // Маршрут для получения массива ссылок на все хранящиеся изображения
-app.get('/images/:folder',  async (req, res) => {
-  try {
-  const { folder } = req.params;  
-  const imageFiles = await fs.promises.readdir(`./images/${folder}`);
-  const imageUrls = imageFiles.map(file => `http://localhost:3000/images/${folder}/${file}`);
-  res.status(200).json({ imageUrls });
-  } catch (error) {
-  console.log(error);
-  res.status(500).json({ error: 'Failed to fetch imageUrls' });
-  }
+app.get('/images/:folder',  (req, res) => {
+  const { folder } = req.params;
+  const folderPath = path.join(imagesPath, folder);
+  fs.promises.readdir(folderPath)
+    .then(imageFiles => {
+      const imageUrls = imageFiles.map(file => `localhost:3000/image/${folder}/${file}`);
+      res.status(200).json({ imageUrls });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to fetch imageUrls' });
+    });
 });
+
 
 //поск файла изображения, отдает массив названий файлов в которых есть совпадения
 app.get('/:folder/search', (req, res) => {
   const { folder } = req.params;
+  const folderPath = path.join(imagesPath, folder);
     const query = req.query.files;
-    findByName(`./images/${folder}`, query)
+    findByName(folderPath, query)
     .then((files) => {
       if (files && files.length > 0) {
         res.send(files);
@@ -119,10 +114,11 @@ app.get('/:folder/search', (req, res) => {
     })
 });
 
+
 // Маршрут для создания новой папки для хранения изображений
 app.post('/create-folder', (req, res) => {
   const folderName = req.body.folderName;
-  const folderPath = `./images/${folderName}`;
+  const folderPath = path.join(imagesPath, folderName);
   if (fs.existsSync(folderPath)) {
     res.status(400).json({ message: 'Folder already exists' });
   } else {
@@ -134,30 +130,32 @@ app.post('/create-folder', (req, res) => {
 // Маршрут для удаления папки для хранения изображений
 app.delete("/:folder", (req, res) => {
   const { folder } = req.params;
-  const folderPath = `./images/${folder}`;
-
-try {
-  // Проверяем существует ли папка перед удалением
-  if (fs.existsSync(folderPath)) {
+  const folderPath = path.join(imagesPath, folder);
+  fs.promises.access(folderPath)
+  .then(() => {
     // Рекурсивно удаляем папку и все ее содержимое
-    fs.rmdirSync(folderPath, { recursive: true });
+    return fs.promises.rmdir(folderPath, { recursive: true });
+  })
+  .then(() => {
     res.status(200).json({ message: 'Folder deleted successfully' });
-  } else {
-    res.status(404).json({ message: 'Folder does not exist' });
-  }
-} catch (err) {
-  res.status(500).json({ message: 'Error deleting folder' });
-}
+  })
+  .catch((err) => {
+    if (err.code === 'ENOENT') {
+      res.status(404).json({ message: 'Folder does not exist' });
+    } else {
+      res.status(500).json({ message: 'Error deleting folder' });
+    }
+  });
 });
 
 // Маршрут для получения списка папок, где хранятся изображения
 app.get('/folders', (req, res) => {
-  const folderPath = path.join(__dirname, 'images');
-  const folders = fs.readdirSync(folderPath, { withFileTypes: true })
+  const folders = fs.readdirSync(imagesPath, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name);
   res.status(200).json({ folders });
 });
+
 
 //возвращает форму загрузки изображений
 app.get("/", (_, res) => {
