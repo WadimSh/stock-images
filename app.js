56a44fac4cc615594cd1df8d03d9380185b5d644
WadimSh <wadim11@yandex.ru>
2023-11-29 12:49:28 +0300

    feat: добавлены библиотеки для повышения безопасности приложения

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const rateLimiter = require('./middlewares/rateLimit');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
 const router = require('./routes/index');
+
 const { PORT = 3000 } = process.env;
+
 const app = express();
 
+app.use(cors());
+
+app.use(requestLogger);
+
+app.use(rateLimiter);
+
+app.use(helmet());
+
 app.use(fileUpload());
+
 app.use(express.json());
 
 //возвращает форму загрузки изображений (временная функциональность)
@@ -15,5 +33,11 @@ app.get("/", (_, res) => {
 });
 
 app.use(router);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(errorHandler);
 
 app.listen(PORT);
